55fe1cfd6d67e70c9bb17149e1f547e31d1e5058
Jonathan Brito <devjonathanbrito@gmail.com>
2022-10-17 10:42:21 -0300

    Created class Curso and Mentoria

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.jonathan.desafio.dominio.Curso;
+import br.com.jonathan.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("A linguagem numero 01 para backend");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("A linguagem numero 01 para frontend");
+        curso2.setCargaHoraria(15);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Repassando as vantagens e desvantagens da linguagem Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
